2ef5b395f27359d373769ee206119dd2943ecbad
MertRamazanEmek <mertramazanemek@gmail.com>
2025-05-17 19:04:52 +0300

    add fixed

--- app/src/main/java/com/example/saytahmin/MainActivity.kt
+++ app/src/main/java/com/example/saytahmin/MainActivity.kt
@@ -1,27 +1,36 @@
 package com.example.saytahmin
 
+import android.content.Intent
 import android.os.Bundle
-import android.renderscript.ScriptGroup.Binding
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.saytahmin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: Binding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = .inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
         enableEdgeToEdge()
 
-        setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+    }
+    fun tikla (view : View)
+    {
+        val intent = Intent(this@MainActivity, PredictActivity::class.java)
+        startActivity (intent)
     }
 }
