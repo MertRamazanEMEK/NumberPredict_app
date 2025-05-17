b30b1717bd099eb18712c51e4dacf423e7a79804
MertRamazanEmek <mertramazanemek@gmail.com>
2025-05-17 19:05:26 +0300

    add fixed

--- app/src/main/java/com/example/saytahmin/PredictActivity.kt
+++ app/src/main/java/com/example/saytahmin/PredictActivity.kt
@@ -1,4 +1,62 @@
 package com.example.saytahmin
 
-class PredictActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.saytahmin.databinding.ActivityMainBinding
+import com.example.saytahmin.databinding.ActivityPredictBinding
+import kotlin.random.Random
+
+class PredictActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPredictBinding
+    private var rastgelesayi = 0
+    private var sayac = 5
+    var tahmin : Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPredictBinding.inflate(layoutInflater)
+        val view = binding.root
+
+
+        rastgelesayi = Random.nextInt(101)
+        setContentView(view)
+
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+        }
+
+    }
+    fun tahminet (view : View)
+    {
+        tahmin = binding.editTextNumber2.text.toString().toIntOrNull()
+        sayac -= sayac
+
+
+        if (tahmin == rastgelesayi)
+        {
+            //val intent = Intent(this@PredictActivity,SonucActivity::class.java)
+        }
+        if ( tahmin!! >rastgelesayi) {
+            binding.tahminText.text = "Azalt"
+            binding.textView.text = "Kalan Hak: $sayac"
+        }
+        if ( tahmin!! < rastgelesayi)
+        {
+            binding.tahminText.text = "Arttır"
+            binding.textView.text = "Kalan Hak: $sayac"
+        }
+        if (sayac == 0)
+        {
+
+        }
+    }
 }
